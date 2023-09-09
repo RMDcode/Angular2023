bda6c975bdcb597da9c8151bbd5f3bd5dab12551
RMDcode <dhurir163@gmail.com>
2023-09-10 00:47:43 +0530

    First Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RestaurentDashComponent } from './restaurent-dash/restaurent-dash.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ApiService } from './shared/api.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RestaurentDashComponent,
+    LoginComponent,
+    SignupComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
